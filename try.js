e25b5df52eed4bd2072712d50b94ab81b6188fff
Karan Kapoor <kk8658141@gmail.com>
2022-06-24 12:50:15 +0530

    add the files

---
+++ try.js
@@ -0,0 +1,15 @@
+const apple =(par) =>
+{
+    console.log(par);
+}
+
+// apple("apple");
+
+(function apple()
+{
+    console.log("apple");
+})();
+
+let apple2 ="apple";
+
+console.log(apple2.concat(" mango"));
