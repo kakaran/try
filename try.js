921d030da4bd6aef79aa19ae8d5ff10619f69c60
Karan Kapoor <kk8658141@gmail.com>
2022-07-01 23:36:43 +0530

    update the all files

--- try.js
+++ try.js
@@ -1,15 +1,26 @@
-const apple =(par) =>
+// const apple =(par) =>
+// {
+//     console.log(par);
+// }
+
+// // apple("apple");
+
+// (function apple()
+// {
+//     console.log("apple");
+// })();
+
+// let apple2 ="apple";
+
+// console.log(apple2.concat(" mango"));
+
+
+
+function MaxCheck(num1,num2)
 {
-    console.log(par);
+    return (num1>num2) ? `${num1} is Maxmimum Number and ${num2} is the Minimum Number` :
+     `${num2} is Maxmimu Number ${num1} is the Minimum Number`;
 }
 
-// apple("apple");
-
-(function apple()
-{
-    console.log("apple");
-})();
-
-let apple2 ="apple";
-
-console.log(apple2.concat(" mango"));
+let value = MaxCheck(34,40);
+console.log(value);
